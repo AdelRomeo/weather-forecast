6fb4692968a22c25ae65ca1638fdcba07a62003c
AdelRomeo <11cage11@mail.ru>
2021-12-27 01:10:05 +0700

    change city name

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -7,15 +7,20 @@ export default function Search({getInfoWeather}) {
   const [cityName, setCityName] = useState("");
 
   //отправка запроса на сервер и получение данных о погоде
-  const getInfo = async (city) => {
+  const getInfo = async (event, city) => {
+    event.preventDefault()
+
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7185b8463e509a9f3b3686b5007206ee`
     );
-    getInfoWeather(response.data.main);
+
+    //выбиарем из ответа сервера нужные данные
+    const currentData = {...response.data.main, ...response.data.wind, cityName}
+    getInfoWeather(currentData);
   };
 
   return (
-    <div className="Search">
+    <form className="Search">
       <input
         placeholder="Введите название города"
         type="text"
@@ -23,7 +28,7 @@ export default function Search({getInfoWeather}) {
         value={cityName}
         onChange={(event)=>setCityName(event.target.value)}
       />
-      <button className="Search_send" onClick={()=>getInfo(cityName)}>ПОСМОТРЕТЬ ПОГОДУ</button>
-    </div>
+      <button className="Search_send" onClick={(event)=>getInfo(event, cityName)}>ПОСМОТРЕТЬ ПОГОДУ</button>
+    </form>
   );
 }
