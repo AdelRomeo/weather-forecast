e2d6dbd05ad17ea6eafbb7c0072d4f16c09a399e
AdelRomeo <11cage11@mail.ru>
2022-01-02 01:08:36 +0700

    component: Error, Loader, Waiting. adaptive

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,23 +2,47 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
 
-export default function Search({getInfoWeather}) {
+export default function Search({
+  getInfoWeather,
+  getShowLoader,
+  getShowError,
+}) {
   //название и изменение названия города
   const [cityName, setCityName] = useState("");
 
   //отправка запроса на сервер и получение данных о погоде
   const getInfo = async (event, city) => {
-    event.preventDefault()
+    event.preventDefault();
+    //показываем лоадер
+    getShowLoader(true);
 
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7185b8463e509a9f3b3686b5007206ee`
-    );
+    try {
+      //отправляем запрос на сервер
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7185b8463e509a9f3b3686b5007206ee`
+      );
+      //скрываем лоадер
+      getShowLoader(false);
+      //очищаем название города
+      setCityName("");
+      //объект для отображения даных
+      const currentData = {
+        ...response.data.main,
+        ...response.data.wind,
+        id: response.data.id,
+        cityName,
+      };
+      //отправляем отбъет с данными
+      getInfoWeather(currentData);
+      //скрываем ошибку если она была
+      getShowError(false);
 
-    setCityName("")
-
-    //выбиарем из ответа сервера нужные данные
-    const currentData = {...response.data.main, ...response.data.wind, id: response.data.id, cityName}
-    getInfoWeather(currentData);
+    } catch (e) {
+      //показываем ошибку
+      getShowError(true);
+      //скрываем лоадер
+      getShowLoader(false);
+    }
   };
 
   return (
@@ -28,9 +52,14 @@ export default function Search({getInfoWeather}) {
         type="text"
         className="Search_input"
         value={cityName}
-        onChange={(event)=>setCityName(event.target.value)}
+        onChange={(event) => setCityName(event.target.value)}
       />
-      <button className="Search_send" onClick={(event)=>getInfo(event, cityName)}>ПОСМОТРЕТЬ ПОГОДУ</button>
+      <button
+        className="Search_send"
+        onClick={(event) => getInfo(event, cityName)}
+      >
+        ПОСМОТРЕТЬ ПОГОДУ
+      </button>
     </form>
   );
 }
---
+++ src/components/waiting/Waiting.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Waiting.css'
+
+export default function Waiting() {
+  return (
+    <div className="Waiting">
+      <h3 className="Waiting_title">Список интересующих городов пуст</h3>
+    </div>
+  );
+}
